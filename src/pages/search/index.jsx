f3d5765e904c27d8db587e35996ca3e72eed84e2
SanuRaj-K <sanusrk29@gmail.com>
2024-11-22 13:37:25 +0530

    feat:icons changed

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -10,39 +10,37 @@ const Search = () => {
 
   const handleGetInsights = () => {
     const toastId = toast.loading("Loading...");
-  
+
     if (domainName.length < 1) {
       toast.error("Please enter your URL");
-      toast.dismiss(toastId); 
+      toast.dismiss(toastId);
     } else {
       const regex = /^(ftp|http|https):\/\/[^ "]+$/;
       if (!regex.test(domainName)) {
-        toast.error("Invalid URL. Please enter a valid domain name.", {
-          id: toastId,
-        });
-        toast.dismiss(toastId); 
+        toast.error("Invalid URL. Please enter a valid domain name.");
+        toast.dismiss(toastId);
         return;
       }
     }
-  
+
     axios
       .post("/add", { domainName })
       .then((res) => {
         if (res.status === 200) {
-          toast.dismiss(toastId); 
+          toast.dismiss(toastId);
           navigate("/table");
         } else {
           toast.error("Something went wrong...");
-          toast.dismiss(toastId); 
+          toast.dismiss(toastId);
         }
       })
       .catch((err) => {
         console.log("err", err);
         toast.error("Domain name is already searched");
-        toast.dismiss(toastId); 
+        toast.dismiss(toastId);
       });
   };
-  
+
   return (
     <div className=" h-screen w-full flex justify-center items-center">
       <div className=" shadow-lg p-5 w-[600px] mx-auto flex-initial items-center justify-center flex-col">
